03a7831f9c1b7b60d55d1c3c07c1bca7e2539d0f
Alexandra Seppänen <alex.seppanen@gmail.com>
2016-09-29 10:36:08 +0200

    Added color to error messaged

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,15 +7,20 @@
 	LunchCheckController.$inject = ['$scope'];
 	function LunchCheckController($scope) {
 		$scope.message = "";
+		$scope.status = "green";
 
 		$scope.checkIfTooMuch = function() {
 			if(!$scope.words || $scope.words.length === 0) {
 				$scope.message = "Please enter data first";
+				$scope.status = "red";
+			} else {
+				$scope.status = "green";
+				if($scope.words.split(',').length > 3) {
+					$scope.message = "Too much!";
+				} else {
+					$scope.message = "Enjoy!";
+				}
 			}
-			else if($scope.words.split(',').length > 3) {
-				$scope.message = "Too much!";
-			} else $scope.message = "Enjoy!";
-			console.log($scope.words.split(',').length);
 		};
 	}
 
